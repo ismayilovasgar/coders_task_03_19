0e0b31d997d76047151ee92c15e4dfc3f855ec1d
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-03-25 11:48:38 +0400

    update_focus_effect

---
+++ app.js
@@ -0,0 +1,10 @@
+const input = document.getElementsByClassName("input_subscribe");
+const input_bar = document.getElementsByClassName("input_bar");
+
+function myFunction() {
+  input_bar[0].style.border = "1px solid red";
+}
+
+function myFunctionOut() {
+  input_bar[0].style.border = "1px solid #181717";
+}
